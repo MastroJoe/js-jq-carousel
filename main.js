e2dfab45e15f2b03b70cba2229e9323385819589
Giorgio <giorgio.pignattari@gmail.it>
2021-03-11 16:01:03 +0100

    variabili JS

---
+++ main.js
@@ -0,0 +1,18 @@
+// Consegna
+// Creare uno slider di immagini: potete usare le immagini che desiderate.
+// Per facilitarvi la vita usate immagini delle stesse dimensioni
+ // Lo slider prevederà due frecce (icone) che permettono di scorrere
+ // tra le immagini dello slider In oltre per scorrere le immagini permettere
+ // anche l’uso delle frecce sinistra e destra della tastiera.
+ // Utiliziamo una classe first e last per capire quali sono la prima e ultima immagine
+ // dello slider Utilizziamo una classe active per aiutarci a capire quale è l’immagine attuale da visualizzare nello slider.
+
+// BONUS
+// Clicchiamo sui pallini e mostriamo l’immagine corrispondente Generiamo i pallini con JS
+
+// creo le variabili degli elementi dell'HTML che mi servono
+$(document).ready(function () {
+  var angleLeft = $(".prev > i");
+  var angleRight = $(".next > i");
+  var images = $(".images > img")
+});
